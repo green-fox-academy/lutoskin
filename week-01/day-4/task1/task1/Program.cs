43e52b5ce710a9efb307f901ea8df92bb9ca1a1c
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-10 12:30:13 +0200

    add task1

---
+++ week-01/day-4/task1/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task1
+{
+    class task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Anna!");
+        }
+    }
+}
---
+++ week-01/day-4/visual_s_first/visual_s_first/task1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace visual_s_first
+{
+    public class task1
+    {
+        public task1()
+        {
+        }
+    }
+}
