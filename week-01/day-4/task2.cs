1a6c7ccb87a7a11f4c12254de8fe872eb37b1e1a
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-10 14:03:20 +0200

    added task2

---
+++ week-01/day-4/task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Humpty Dumpty sat on a wall,");
+			Console.WriteLine("Humpty Dumpty had a great fall.");
+			Console.WriteLine("All the king's horses and all the kings men");
+			Console.WriteLine("Couldn't put Humpty Dumpty together again.");
+        }
+    }
+}
