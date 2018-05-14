a6eef8c17cb033e7666d6d367a972a702f056f9a
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-14 23:10:27 +0200

    add task12

---
+++ week-02/day-1/task12.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task12_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //12 matrix
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+
+            int[,] matrix = new int[4, 4];
+         
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					if (i == j)
+                    {
+                        matrix[i,j] = 1;
+                    }
+				}  
+               
+			}
+            Console.WriteL(matrix[i, j] + " ");
+		}
+    }
+}
