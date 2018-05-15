f1c81c4ebac1e83ccac47293cea14ec4cc2ea6a7
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-15 16:15:30 +0200

    add task8

---
+++ week-02/day-2/task8appenda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace w2d2t8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Create a method called AppendA() that adds "a" to every string in the far list.
+            // The parameter should be a list.
+
+            Console.WriteLine(AppendA(far));
+            // Expected output: "kutya", "macska", "kacsa", "róka", "halacska"
+        }
+
+		public static StringBuilder AppendA(List<string> far)
+		{
+			StringBuilder appendAFar = new StringBuilder();
+			for (int i = 0; i < far.Count; i++)
+			{
+				appendAFar.Append(far[i] + "a ");
+			}
+			return appendAFar;
+		}
+    }
+}
