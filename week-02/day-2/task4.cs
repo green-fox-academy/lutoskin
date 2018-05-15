d0e28bb5f5e4dda17220ac013f21f72f3af68f8a
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-15 12:55:56 +0200

    add task4

---
+++ week-02/day-2/task4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace w2d2t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string todoText = " - Buy milk\n";
+			// Add "My todo:" to the beginning of the todoText
+			// Add " - Download games" to the end of the todoText
+			// Add " - Diablo" to the end of the todoText but with indentation
+
+			// Expected output:
+
+			// My todo:
+			//  - Buy milk
+			//  - Download games
+			//      - Diablo
+            
+			todoText = todoText.Insert(0, "My todo:\n");
+
+			StringBuilder listBuilder = new StringBuilder(todoText);
+			listBuilder = listBuilder.Append(" - Download games\n");
+			listBuilder = listBuilder.Append("\t- Diablo");
+         
+			Console.WriteLine(listBuilder);
+        }
+
+    }
+}
