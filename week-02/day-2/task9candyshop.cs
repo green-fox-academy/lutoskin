3402dd71488832cc5f068a2204c24487794eeb7e
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-15 16:53:46 +0200

    add task9

---
+++ week-02/day-2/task9candyshop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace w2d2t9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list.
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+            // Create a method called Sweets() which takes the list as a parameter.
+
+            Console.WriteLine(Sweets(list));
+            // Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream";
+        }
+        
+		public static string Sweets(List<object> sweetList)
+		{
+			sweetList[1].ToString();
+			sweetList[1] = "Croissant";
+			sweetList[3].ToString();
+			sweetList[3] = "Ice cream";
+			string correctSweets = "";
+			foreach (var sweet in sweetList)
+			{
+				correctSweets += sweet + "\n";
+			}
+			return correctSweets;
+		}
+    }
+}
