d72636c42d67d0dfc7cbe2aabe45c855655ab8e7
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-16 14:30:40 +0200

    add practice1-3

---
+++ week-02/day-3/p1dividebyzero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace w2d2p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Create a function that takes a number
+			// divides ten with it,
+			// and prints the result.
+			// It should print "fail" if the parameter is 0
+			Console.WriteLine(FunctionDivideTen());
+		
+		}
+        
+		public static string FunctionDivideTen()
+		{
+			int a = 10;
+            Console.WriteLine("Please give me a number");
+            int b = Int32.Parse(Console.ReadLine());
+            try
+            {
+                int c = a / b;
+				return c.ToString();
+            }
+            catch (DivideByZeroException)
+            {
+				return "fail";
+            }
+            
+		}
+    }
+}
---
+++ week-02/day-3/p2printeachline.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace w2d3p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Write a program that opens a file called "my-file.txt", then prints
+			// each of lines form the file.
+			// If the program is unable to read the file (for example it does not exists),
+			// then it should print an error message like: "Unable to read file: my-file.txt"
+			OpenMyFile("my-file.txt");
+
+        }
+
+		static void OpenMyFile(string something)
+		{
+			string data = "";
+			try
+			{
+				StreamReader sr = new StreamReader("my-file.txt");
+				while (data != null)
+                {
+                    data = sr.ReadLine();
+                    if (data != null)
+                    {
+                        Console.WriteLine(data);
+                    }
+                }
+			}
+			catch (IOException)
+			{
+				Console.WriteLine("unable"); 
+			}   
+		}
+    }
+}
