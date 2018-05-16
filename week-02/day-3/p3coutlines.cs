1b103147e37ced6e9e0fd31be0e02c6a1cd385e2
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-16 14:56:23 +0200

    correct p3

---
+++ week-02/day-3/p3coutlines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace w2d3p3forreal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Write a function that takes a filename as string,
+			// then returns the number of lines the file contains.
+			// It should return zero if it can't open the file, and
+			// should not raise any error.      
+
+			Console.WriteLine(CountLines("file.txt"));
+		}
+
+		static int CountLines(string filePath)
+		{
+            try
+			{
+				return File.ReadLines(filePath).Count();            
+			}
+			catch (IOException)
+			{
+				return 0;
+			}
+		}
+    }
+}
