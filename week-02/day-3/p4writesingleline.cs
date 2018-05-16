da129e120c6a755b63e33128f5af83629963094e
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-16 15:21:20 +0200

    add p4

---
+++ week-02/day-3/p4writesingleline.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace w2d3p4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Open a file called "my-file.txt"
+			// Write your name in it as a single line
+			// If the program is unable to write the file,
+			// then it should print an error message like: "Unable to write file: my-file.txt"
+			Console.WriteLine(WriteSingleLine("anna"));
+			Console.WriteLine(File.ReadAllText("my-file.txt"));
+			Console.ReadLine();
+
+		}
+
+		static string WriteSingleLine(string something)
+		{
+            try
+			{
+				StreamWriter sw = new StreamWriter("my-file.txt");
+				sw.Write(something);
+				sw.Dispose();
+				return "ok, done";   
+			}
+
+			catch (Exception)
+			{
+				return "unable to";
+			}
+
+		}
+    }
+}
