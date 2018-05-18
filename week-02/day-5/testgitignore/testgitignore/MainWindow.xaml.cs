f92952ec284a5bf1012cf9675751c524393b8f8a
lutoskin <anna.lutoskin@gmail.com>
2018-05-18 10:16:46 +0200

    gitignore testing

---
+++ week-02/day-5/testgitignore/testgitignore/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using GreenFox;
+using System.Windows;
+using System.Windows.Media;
+
+namespace testgitignore
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawDiagonals(foxDraw);
+        }
+
+        public static void DrawDiagonals(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.ForestGreen);
+            foxDraw.DrawLine(0, 0, 600, 600);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(600, 0, 0, 600);
+        }
+    }
+}
