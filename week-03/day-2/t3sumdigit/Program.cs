d0aa5083e5d199e001eb0b272a733cdc88e1d41f
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-24 12:08:20 +0200

    add sumdigits

---
+++ week-03/day-2/t3sumdigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace t3sumdigit
+{
+    class Program
+    {
+		public static void Main() 
+		{
+            Console.WriteLine(SumDigit(2456));
+        }
+
+        public static int SumDigit(int n)
+		{
+			if (n < 10)
+			{
+				return n;
+			}
+            
+			else
+			{
+				return n%10	+ SumDigit(n/10);
+			}
+		}
+    }
+}
