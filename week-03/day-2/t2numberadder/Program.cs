d21bf9788b8c9fd3125307b0fee87679846f7714
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-24 11:23:36 +0200

    numberadder

--- week-03/day-2/t2numberadder/Program.cs
+++ week-03/day-2/t2numberadder/Program.cs
@@ -6,7 +6,23 @@ namespace t2numberadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			int n;
+            Console.WriteLine("Give me a number");
+			n = Int32.Parse(Console.ReadLine());
+			Console.WriteLine(NumberAdder(n));
         }
+
+		public static int NumberAdder(int n)
+		{
+			if (n == 1)
+			{
+				return + 1;
+			}
+
+			else
+			{
+				return n + NumberAdder(n - 1);
+			}
+		}
     }
 }
