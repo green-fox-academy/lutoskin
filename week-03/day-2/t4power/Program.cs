3b947b65677e21657c2e47bb6994a6bd9e9c2e2e
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-24 12:23:48 +0200

    add power

---
+++ week-03/day-2/t4power/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace t4power
+{
+    class Program
+    {
+		static void Main()
+        {
+            Console.WriteLine(Power(2,3));
+        }
+
+        public static int Power(int baseNumber, int power)
+		{
+			if (power == 0)
+			{
+				return 1;
+			}
+
+			else
+			{
+				return baseNumber * Power(baseNumber, power - 1);
+			}	
+		}
+    }
+}
