676225680120efac1b124aac35c3499623955500
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-24 10:49:42 +0200

    counter

---
+++ week-03/day-2/t1counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace t1counter
+{
+	class Program
+	{
+
+		static void Main(string[] args) {
+			int n;
+            Console.WriteLine("Give me a number");
+			n = Int32.Parse(Console.ReadLine());
+			Console.WriteLine(Recursion(n));
+		}      
+        
+		public static int Recursion(int n)
+		{
+			if (n == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				Console.WriteLine(n);
+				return  Recursion(n - 1);
+			}
+		}
+	}
+}
+
+
+        
+   
+
