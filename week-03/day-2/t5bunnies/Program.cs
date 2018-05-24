4ec0d894f02b7145a7a6ba397e155aeb32a9bef4
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-24 12:47:11 +0200

    t5bunnies

---
+++ week-03/day-2/t5bunnies/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace t5bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine(BunniesEars(18));
+        }
+
+        public static int BunniesEars(int numberOfBunnies)
+		{
+			if (numberOfBunnies == 1)
+			{
+				return 2;
+			}
+
+			else
+			{
+				return 2 + BunniesEars(numberOfBunnies - 1);
+			}
+		}
+    }
+}
