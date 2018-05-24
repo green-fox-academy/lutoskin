d0f2e3a34ffa4da74a3d5c092151e330b0b38b9d
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-24 17:46:24 +0200

    strings3

---
+++ week-03/day-2/t9stringsagainandagain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace t9stringsagainandagain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine(CompleteString("spacex"));
+        }
+
+		public static string CompleteString(string textIn)
+		{
+			if (textIn == "")
+			{
+				return null;
+			}
+			if (textIn.Length == 1)
+			{
+				return textIn[0].ToString();
+			}
+
+			return textIn[0] + "*" + CompleteString(textIn.Remove(0,1));
+		}
+	}
+}
+// Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a "*".
