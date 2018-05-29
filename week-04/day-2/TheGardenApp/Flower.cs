2594d2f03bbf0a26bcb95428c4d21a76676fa48d
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-29 17:28:17 +0200

    gardenapp: make list of thirsty plants

--- week-04/day-2/TheGardenApp/Flower.cs
+++ week-04/day-2/TheGardenApp/Flower.cs
@@ -3,24 +3,25 @@ namespace TheGardenApp
 {
 	public class Flower : Garden
     {      
-		public void Info(string color)
-		{
+        public Flower(string color)
+        {
+            this.color = color;
+            water = 0;
+        }
+        
+		public void Info()
+        {
             if (water < 5)
             {
                 Console.WriteLine("The {0} flower needs water.", color);
             }
-			Console.WriteLine("The {0} flower doesn't need water.", color);
-		}
-      
-		public Flower(string color, double water)
-		{
-			this.color = color;
-			water = 0;
-		}
+            Console.WriteLine("The {0} flower doesn't need water.", color);
+        }
         
-		public double Water(double amountOfWater)
+		public void Water(double amountOfWater)
         {
-            return water += 0.75 * amountOfWater;
+            water += 0.75 * amountOfWater;
+
         }
 
     }
--- week-04/day-2/TheGardenApp/Garden.cs
+++ week-04/day-2/TheGardenApp/Garden.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TheGardenApp
 {
     public class Garden
     {
-        public Garden()
+		protected double water;
+		protected string color;
+		int amountOfWater;
+		private List<Garden> thirstyPlants = new List<Garden>();
+
+		public void AddFlower(Flower addFlower)
+		{
+			if (water <= 0)
+			{
+				thirstyPlants.Add(addFlower);
+			}
+		}
+
+		public void AddTree(Tree addTree)
         {
+            if (water <= 0)
+            {
+				thirstyPlants.Add(addTree);
+            }
         }
+
+		public void Water(int amountOfWater)
+		{
+			Console.WriteLine("Watering with " + amountOfWater);
+		}
     }
 }
