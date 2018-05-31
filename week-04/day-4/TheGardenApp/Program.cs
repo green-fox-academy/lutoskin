7f9966741aecd445d2c62d36b0ac15d5b6ebdeeb
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-05-31 16:46:42 +0200

    add(gardenapp)

---
+++ week-04/day-4/TheGardenApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheGardenApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         foreach (var plant in plants)
+			{
+				plant.IsItThirsty();
+			}
+         
+		}
+    }
+}
