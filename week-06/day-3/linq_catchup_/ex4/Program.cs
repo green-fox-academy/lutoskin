17da645e71962e6f0c43230ef21147aeaa55ac5f
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-06-20 17:27:31 +0200

    add(ex4)

---
+++ week-06/day-3/linq_catchup_/ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ex4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+			var sqrGreaterThanTw = n.Where(num => Math.Pow(num, 2) > 20);
+
+			foreach (var num in sqrGreaterThanTw)
+			{
+                Console.WriteLine(num);
+			}
+		}
+    }
+}
+
