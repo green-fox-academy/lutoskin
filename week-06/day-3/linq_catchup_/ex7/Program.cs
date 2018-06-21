0c2bbe6aa77f4d2f7bed4d2f86137fdc9ff14464
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-06-21 17:06:02 +0200

    add(ex7)

---
+++ week-06/day-3/linq_catchup_/ex7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ex7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			string[] cities = { "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS" };
+
+			var aiCities = from city in cities
+						   where city[0] is 'A' && city[city.Length - 1] is 'I'
+						   select city;
+
+			foreach (var city in aiCities)
+			{
+                Console.WriteLine(city);
+			}
+		}
+    }
+}
+
+// Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
