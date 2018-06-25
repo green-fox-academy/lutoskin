6370c6b5ac94affc21dc130b93b7286d72b81f80
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-06-25 16:41:56 +0200

    add(helloworld)

---
+++ week-07/helloworld/helloworld/Controllers/RESTController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using helloworld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        static int counter;
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {           
+            counter++;
+
+            Greeting greeting = new Greeting()
+            {
+                Id = counter,
+                Content = "Hello " + name               
+            };
+            return Ok(greeting);
+                             
+        }
+    }
+}
---
+++ week-07/helloworld/helloworld/Controllers/WebController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using helloworld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    public class WebController : Controller
+    {
+        static int counter;
+
+        public IActionResult Greeting(string name)
+        {
+            counter++;
+
+            var greeting = new Greeting()
+            {
+                Id = counter,
+                Content = name
+            };
+
+            return View(greeting);
+        }
+    }
+}
