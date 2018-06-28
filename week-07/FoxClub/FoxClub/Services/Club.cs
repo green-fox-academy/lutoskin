6b1686fdfb2095da360a6c134d0c0fd628dcea3a
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-06-28 16:14:18 +0200

    add(FoxClub)

---
+++ week-07/FoxClub/FoxClub/Services/Club.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoxClub.Models;
+
+namespace FoxClub.Services
+{
+    public class Club : IClub
+    {
+        public List<Fox> foxes;
+
+        public Club()
+        {
+            foxes = new List<Fox>();
+        }
+
+        public void AddFox(Fox fox)
+        {
+            foxes.Add(fox);
+        }
+
+        public List<Fox> GetFoxes()
+        {
+           return foxes;
+        }
+
+        public Fox GetLatestFox()
+        {
+            return foxes[foxes.Count - 1];
+        }
+    }
+}
