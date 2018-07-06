b8371bd076ddc76a96f8ad83e43d24453a13edc0
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-07-06 11:35:03 +0200

    func(reddit): di

--- week-08/Reddit/Reddit/Controllers/RedditController.cs
+++ week-08/Reddit/Reddit/Controllers/RedditController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Reddit.Services;
 
 namespace Reddit.Controllers
 {
     public class RedditController : Controller
     {
-        public IActionResult Index()
+        private RedditService redditService;
+
+        public RedditController(RedditService redditService)
+        {
+            this.redditService = redditService;
+        }
+
+        public IActionResult Trending()
         {
             return View();
         }
--- week-08/Reddit/Reddit/Services/RedditService.cs
+++ week-08/Reddit/Reddit/Services/RedditService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reddit.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace Reddit.Services
 {
     public class RedditService
     {
+        private RedditContext redditContext;
+
+        public RedditService(RedditContext redditContext)
+        {
+            this.redditContext = redditContext;
+        }
     }
 }
