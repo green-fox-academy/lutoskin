189471790679ba2f8a70b5a78205b1435a512f3e
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-07-06 11:31:50 +0200

    add(reddit)

---
+++ week-08/Reddit/Reddit/Repositories/RedditContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Repositories
+{
+    public class RedditContext : DbContext
+    {
+        public DbSet<RPost> RPosts { get; set; }
+
+        public RedditContext(DbContextOptions options) : base (options)
+        {
+        }
+
+    }
+}
