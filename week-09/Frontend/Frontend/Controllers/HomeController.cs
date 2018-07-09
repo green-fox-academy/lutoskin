b7353834e34624cab8e938d5bf86930063366892
Anna Lutoskin <anna.lutoskin@gmail.com>
2018-07-09 17:18:23 +0200

    add(frontend)

---
+++ week-09/Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using Frontend.Services;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HomeService homeService;
+
+        public HomeController(HomeService homeService)
+        {
+            this.homeService = homeService;
+        }
+
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet("/doubling")]
+        public IActionResult Doubling(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            return Json(new { received = input, result = 2 * input });
+        }
+
+        [HttpGet("/greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null )
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
+        }
+
+        [HttpGet("/appenda/{appendable}")]
+        public IActionResult Appenda(string appendable)
+        {
+            if (appendable == null)
+            {
+                return NotFound();
+            }
+            return Json(new { appended = $"{appendable}a" });
+        }
+
+        [HttpPost("/dountil/{what}")]
+        public IActionResult DoUntil([FromBody]Number number, string what)
+        {
+            if (number.Until == 0)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+            else if (what == "sum")
+            {
+                return Json(new { result = homeService.Sum(number) });
+            }
+            else if (what == "factor")
+            {
+                return Json(new { result = homeService.Factorial(number) });
+            }
+            
+            return Json(new { error = "Please provide a number!" });
+        }
+    }
+}
---
+++ week-09/Frontend/Frontend/Services/HomeService.cs
@@ -0,0 +1,31 @@
+﻿using Frontend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Services
+{
+    public class HomeService
+    {
+        public int Sum(Number number)
+        {
+            int sum = 0;
+            for (int i = 1; i <= number.Until; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public int Factorial(Number number)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number.Until; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
